aa817fc682b4280f0440674006f41672afdfbe7e
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-10 19:36:14 +0100

    Creation de la classe Pi

---
+++ src/Pi.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Pi implements PiInterface {
+	ArrayList<Pi>.piList;
+	public Pi(){
+		piList = new ArrayList <Pi>()
+	}
+	public int ameliorationDistance(){
+		
+	}
+
+}
