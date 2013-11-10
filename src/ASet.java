de063e081cb032d53b1ef9d5e47a63201574aff5
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-10 18:04:49 +0100

    Ajout des methodes estdansA et rajoutedansA

--- src/ASet.java
+++ src/ASet.java
@@ -3,8 +3,20 @@ import java.util.ArrayList;
 public class ASet implements ASetInterface {
 	private ArrayList<Vertex> vertexList;
 
-	public Aset() {
+	public ASet() {
 		vertexList = new ArrayList<Vertex>();
 	}
-
+	public boolean estDansA(){
+		for (Vertex X : vertexList) {
+			if (this.equalsTo(X)==true) return true;
+		}
+		return false;
+	}
+		
+public void rajouteDansA(){
+	vertexList.add(Vertex this.Vertex);
+		
+	}
+		
+    
 }
