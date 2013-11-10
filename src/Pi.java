dfa391087bb4c37a33c94fb31423e56e6326e340
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-10 19:39:14 +0100

    Création des getters/setters

--- src/Pi.java
+++ src/Pi.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
 public class Pi implements PiInterface {
 	ArrayList<Pi> piList;
+	private int distance ;
 	public Pi(){
 		piList = new ArrayList <Pi>();
 	}
+	
+	public final int getDistance() {
+		return distance;
+	}
+
+	public final void setDistance(int distance) {
+		this.distance = distance;
+	}
+
 	public int ameliorationDistance(){
 		
 	}
