a117d103da8a4c6a698e4074402a44f087b551ac
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-10 18:29:37 +0100

    Création des méthodes utiles pour les méthodes dans ASet

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,3 +1,19 @@
 public class Vertex {
+   private int indice ;
+   
+	   
+   public final int getIndice() {
+	return indice;
+}
+   
 
+public final void setIndice(int indice) {
+	this.indice = indice;
+}
+
+
+   
+	public boolean equalsTo(Vertex anotherVertex){
+	 return (indice == anotherVertex.getIndice());
+	}
 }
